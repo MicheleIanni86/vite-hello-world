a3165041e86f05cad48b074b5e19a26f205b5242
MicheleIanni86 <micheleianni86@gmail.com>
2024-02-07 16:35:18 +0100

    feat: Add AppTitle with App.vue

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,11 @@ export default {
 </script>
 
 <template>
-  <h1>{{ titolo }}</h1>
+  <h1 class="title">{{ titolo }}</h1>
 </template>
 
-<style></style>
+<style>
+@import url('../src/assets/main.css');
+
+
+</style>
